68ba19dc3e6227fedcc81825a5f0a4f36346393a
vidallizet <lvidalsaldivar7855@richmondhillhs.org>
2022-01-12 16:42:41 +0000

    streetchanges

--- src/App.js
+++ src/App.js
@@ -8,9 +8,49 @@ function App() {
     <div className="App">
       <Navbar/>
       <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
+    <DataView
+  borough="Staten Island"
+  renterCost="$14,292"
+  ownerCost="$29,752"
+  renterIncome="$37,882"
+  ownerIncome="$94,177"/>
 
-      <DataView/>
-    </div>
+
+ <DataView
+  borough="Queens"
+  renterCost="$16,812"
+  ownerCost="$29,256"
+  renterIncome="$46,549"
+  ownerIncome="$80,144"/>
+ 
+
+ <DataView
+  borough="Manhattan"
+  renterCost="$18,900"
+  ownerCost="$36,252"
+  renterIncome="$62,173"
+  ownerIncome="$142,046"/>
+
+
+ <DataView
+  borough="Bronx"
+  renterCost="$13,176"
+  ownerCost="$28,248"
+  renterIncome="$29,302"
+  ownerIncome="$76,454"/>
+
+
+ <DataView
+  borough="Brooklyn"
+  renterCost="$15,144"
+  ownerCost="$31,908"
+  renterIncome="$40,128"
+  ownerIncome="$83,747"/>
+  
+
+ 
+
+         </div>
   );
 }
 
--- src/components/DataView.js
+++ src/components/DataView.js
@@ -15,6 +15,12 @@ function DataView(props) {
             <div className="text">
               <h3 className="text-header">Renters</h3>
             </div>
+            
+            <div className="text bottom">
+  <h2 className="text-data">{props.renterIncome}</h2>
+  <h3 className="text-label">Annual Income</h3>
+</div>
+
             <div className="text">
               <h2 className="text-data">{props.renterCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
@@ -24,8 +30,14 @@ function DataView(props) {
             <div className="text">
               <h3 className="text-header">Homeowners</h3>
             </div>
+
+            <div className="text bottom">
+  <h2 className="text-data">{props.ownerIncome}</h2>
+  <h3 className="text-label">Annual Income</h3>
+</div>
+
             <div className="text">
-              <h2 className="text-data">{}</h2>
+              <h2 className="text-data">{props.ownerCost}</h2>
               <h3 className="text-label">Annual Housing Cost</h3>
             </div>
           </div>
